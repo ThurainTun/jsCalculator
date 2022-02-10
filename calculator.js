625f0245a6fb2ca33e481b8b471a1ff49db9de7e
ThurainTun <stp7112004@gmail.com>
2022-02-10 23:05:59 +0630

    first_commit

---
+++ calculator.js
@@ -0,0 +1,19 @@
+let result = document.getElementById("inputext");
+
+let calculate = number => result.value = result.value + number;
+
+// let Result = () => {
+//     try {
+//         result.value = eval(result.value)
+//     } catch (error) {
+//         alert("Enter the valid result")
+//     }
+// }
+
+// function clr() {
+//     result.value = ""
+// }
+
+// function del() {
+//     result.value = result.value.slice(0, -1)
+// }
